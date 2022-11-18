fa582d731ca4a2c40f3c8e85b7b8a5bf152e930c
mpgcode <86495198+mpgcode@users.noreply.github.com>
2022-11-18 08:07:51 -0700

    MPG - Initial Commit

---
+++ WCF/WcfServiceLibrary/WcfServiceClient1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WCF = ServiceReference1;
+
+namespace WcfServiceClient1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            WCF.Service1Client proxy = new();
+
+            Task<string> myTask = proxy.GetDataAsync(4);
+            myTask.Wait();
+            string proxyResult = myTask.Result;
+
+            MessageBox.Show(proxyResult);
+
+        }
+    }
+}

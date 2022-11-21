0072a950bd6bb65c39d19598d0ac14385dc90362
mpgcode <86495198+mpgcode@users.noreply.github.com>
2022-11-21 08:38:19 -0700

    MPG - Added Self Hosting Console App

---
+++ WCF/ZzaApp/Zza.SelfHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Services;
+
+namespace Zza.SelfHost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (ServiceHost host = new ServiceHost(typeof(ZzaService)))
+                {
+                    host.Open();
+
+                    Console.WriteLine("Hit any key to exit");
+                    Console.ReadKey();
+
+                    host.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
